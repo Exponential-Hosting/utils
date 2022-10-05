b320d6a8af8ed4e1cecf479e5754632070452d27
Keshavaprasad B S <kbsbng@gmail.com>
2022-10-05 17:40:06 +0530

    Get signature param from header instead of query

--- module/validateCredential.js
+++ module/validateCredential.js
@@ -2,11 +2,11 @@ const md5 = require("md5");
 
 const validateCredential = (apiSecret, req, res, next) => {
   const query = req.url.split('?')[1] || "";
-  const { computedSignature, querySignature } = __computeSignature(apiSecret, query);
+  const computedSignature = computeSignature(apiSecret, query);
 
   // console.log(computedSignature, ' <----> ', signature);
 
-  if (computedSignature === querySignature) {
+  if (computedSignature === req.get('signature')) {
     next();
   } else {
     res.status(401).json({
@@ -16,25 +16,15 @@ const validateCredential = (apiSecret, req, res, next) => {
   }
 };
 
-const __computeSignature = (apiSecret, query) => {
+const computeSignature = (apiSecret, query) => {
   const reqUrlSearchParams = new URLSearchParams(query);
   const params = Object.fromEntries(reqUrlSearchParams.entries());
-  const querySignature = params.signature || "";
-  delete params.signature;
 
   const sortedStr = Object.keys(params)
     .sort()
     .map((paramKey) => `${paramKey}=${params[paramKey]}`)
     .join("|");
   const computedSignature = md5(sortedStr + apiSecret).toString();
-  return { computedSignature, querySignature };
-};
-
-const computeSignature = (apiSecret, query) => { 
-  const { computedSignature, querySignature } = __computeSignature(apiSecret, query);
-  if (querySignature !== "") {
-    throw new Error("query should not contain a signature parameter since it should be used to send the signature from the Exponential sever");
-  }
   return computedSignature;
 };
 
